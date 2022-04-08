d508911bb04a83323031918c8d9872aae5c494e7
Nick Tse <nicholashtse@gmail.com>
2022-04-08 13:37:17 -0700

    Add additional picture and Dark/Light Mode button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
-  $("clickable").click(function() {
-    $(".tabby-cat-showing").toggle();
-    $(".tabby-cat-hidden").toggle();
+  $(".clickable").click(function() {
+    $("#tabby-cat-showing").toggle();
+    $("#tabby-cat-hidden").toggle();
+  });
+  $(".fadeIn").click(function() {
+    $("#Seattle-showing").fadeIn();
+    $("#Seattle-hidden").hide();
+  });
+  $(".fadeOut").click(function() {
+    $("#Seattle-showing").fadeOut();
+    $("#Seattle-hidden").show();
+  });
+  $(".btn").click(function() {
+    $("#darkMode").toggle();
+    $("#lightMode").toggle();
+  });
+  $("button#gray").click(function() {
+    $("body").removeClass();
+    $("body").addClass("gray-background");
+  });
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
   });
 });
